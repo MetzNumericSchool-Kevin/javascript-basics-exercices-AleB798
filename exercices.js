ee980e705689aee26865685c45d5ec4dd45296c1
Alexandra Baumann <alexbaumann342@gmail.com>
2025-01-22 09:00:06 +0100

    définition de variables

---
+++ exercices.js
@@ -0,0 +1,11 @@
+// const nom_sorcier = prompt("Comment te nommes-tu, sorcier ? 🧙‍♂️");
+// console.log("Le sorcier se nomme :" + nom_sorcier + " ! 🎉");
+
+// EXO 1 : Définition de variables
+
+const nameShop = "Magimisticalicious";
+let healingPotion = 150;
+let healingPotionPrice = 20;
+let isOpen = true;
+
+
